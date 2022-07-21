38ac9fde685d298fee9c8f277a3d0513a135342b
Jason <jason_052@hotmail.com>
2022-07-21 19:50:48 +0100

    custom

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const path = require('path')
-const moment = require('moment')
-const { HOST } = require('./src/constants')
-const db = require('./src/database')
+const fetch = require('node-fetch');
 
 const PORT = process.env.PORT || 5000
 
@@ -15,41 +13,25 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res) {
-  res.send('Get ready for OpenSea!');
+  res.send('OpenSea API ready for Diddy Dinos!');
 })
-
-app.get('/api/token/:token_id', function(req, res) {
-  const tokenId = parseInt(req.params.token_id).toString()
-  const person = db[tokenId]
-  const bdayParts = person.birthday.split(' ')
-  const day = parseInt(bdayParts[1])
-  const month = parseInt(bdayParts[0])
-  const data = {
-    'name': person.name,
-    'attributes': {
-      'birthday': person.birthday,
-      'birth month': monthName(month),
-      'zodiac sign': zodiac(day, month),
-      // 'age': moment().diff(person.birthday, 'years')
-    },
-    'image': `${HOST}/images/${tokenId}.png`
+async function resolveMetadata(req) {
+  try {
+    const tokenId = parseInt(req.params.token_id).toString();
+    const metadataURI = `https://diddydinos.blob.core.windows.net/metadata/${tokenId}.json`;
+    const response = await fetch(metadataURI);
+    if (response.status < 400) return response.json();
+    return null;
+  } catch (e) {
+    return null;
   }
-  res.send(data)
-})
+}
+app.get('/:token_id', async function (req, res) {
+  const metadata = await resolveMetadata(req);
+  if (metadata) res.send(metadata);
+  else res.sendStatus(404);
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 })
-
-// returns the zodiac sign according to day and month ( https://coursesweb.net/javascript/zodiac-signs_cs )
-function zodiac(day, month) {
-  var zodiac =['', 'Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
-  var last_day =['', 19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
-  return (day > last_day[month]) ? zodiac[month*1 + 1] : zodiac[month];
-}
-
-function monthName(month) {
-  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-  ]
-  return monthNames[month - 1]
-}
