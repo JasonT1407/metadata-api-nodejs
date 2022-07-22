398534240769caa72749a6c35dbc734a56bc7234
Jason <jason_052@hotmail.com>
2022-07-22 15:47:13 +0100

    remove console

--- src/api.js
+++ src/api.js
@@ -29,7 +29,6 @@ async function resolveMetadata(req) {
     const tokenId = parseInt(req.params.token_id).toString();
     const metadataURI = `https://diddydinos.blob.core.windows.net/metadata/${tokenId}.json`;
     const response = await fetch(metadataURI);
-    console.log(response);
     if (response.status < 400) return response.json();
     return null;
   } catch (e) {
