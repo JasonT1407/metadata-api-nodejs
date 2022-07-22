e296cae6f0c8e6b098a5e230f9d94a341ea2c8e9
Jason <jason_052@hotmail.com>
2022-07-22 16:00:04 +0100

    slash on router

--- src/api.js
+++ src/api.js
@@ -50,7 +50,7 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 })
 
-app.use(``, router);
+app.use(`/`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
